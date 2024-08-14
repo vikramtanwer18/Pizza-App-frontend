f4dfe1aeb2a681d6a5922d907f9f21e4e7867816
vikram saini <vikramkumarsaini006@gamil.com>
2024-08-15 00:30:39 +0530

    Add Signup and signin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Home from "./pages/Home"
-import Layout from './Layouts/Layout'
+import Signup from './pages/Auth/Signup'
+import { Route, Routes } from "react-router"
+import Login from "./pages/Auth/Login"
+
 function App() {
  
   return (
-   <div>
-     <Layout>
-        <Home />
-      </Layout>
-   </div>
+   <>
+    <Routes>
+      <Route path="/" element = {<Home/>}/>
+      <Route path="/auth/signup" element = {<Signup/>}/>
+      <Route path='/auth/login' element={<Login />} />
+    </Routes>
+       
+   </>
   )
 }
 
