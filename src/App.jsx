c82c6c59bd4d9f5fd33416c3a8e7f6594f411370
vikram saini <vikramkumarsaini006@gamil.com>
2024-08-15 02:32:35 +0530

    Add Signup logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home"
 import Signup from './pages/Auth/Signup'
 import { Route, Routes } from "react-router"
 import Login from "./pages/Auth/Login"
+import NotFound from "./pages/NotFound"
 
 function App() {
  
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element = {<Home/>}/>
       <Route path="/auth/signup" element = {<Signup/>}/>
       <Route path='/auth/login' element={<Login />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
        
    </>
